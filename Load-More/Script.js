044fe5e801a6044e76b2c814a1e04c7b9fbe7f36
Pranav <pnarang1130@gmail.com>
2019-07-11 18:42:32 -0600

    Added script that clicks on load more buttons and scrolls to bottom of page

---
+++ Load-More/Script.js
@@ -0,0 +1,33 @@
+let numIdeas = document.getElementsByClassName("stat-number brand-color")[0].innerHTML;
+
+ 
+
+var counter = 0;
+
+ 
+
+var looper = setInterval(function(){
+
+    counter++;
+
+    num=document.getElementsByTagName("a").length;
+
+    num -= 7;
+
+    document.getElementsByTagName("a")[num].click();
+
+    window.scrollTo(0,document.body.scrollHeight);
+
+ 
+
+    if (counter >= Math.floor(numIdeas/3))
+
+    {
+
+        clearInterval(looper);
+
+    }
+
+ 
+
+}, 2000);
