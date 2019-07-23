7e217702ee0741f57a246c92adf654105da2d5e0
Pranav Narang <pnarang1130@gmail.com>
2019-07-23 10:16:09 -0600

    sql triggers added

--- 1.sql
+++ 1.sql
@@ -23,3 +23,20 @@ create table terminals (
     terminalMinLevelTEnd bit
 )
 
+
+
+
+CREATE TRIGGER 'validate_line_on_insert'
+    BEFORE INSERT
+    ON 'Pipelines'
+    FOR EACH ROW
+BEGIN
+    IF ( NEW.'Source' NOT IN ( SELECT T.Name from Terminal T) )
+    OR ( NEW.'Destination' NOT IN ( SELECT T2.Name from Terminal T2) )
+    OR NEW.'Bypass_Line' IS NULL
+    THEN
+    SIGNAL SQLSTATE VALUE '45000'
+        SET MESSAGE_TEXT = '[table:Pipelines] - Invalid input data'
+    END IF;
+END;
+
