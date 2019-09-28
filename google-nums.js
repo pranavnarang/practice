9cb10c5ff2ecc173427acd8495f1104892cac5ff
Pranav <pnarang1130@gmail.com>
2019-09-27 22:42:15 -0600

    finished google-nums

--- google-nums.js
+++ google-nums.js
@@ -13,6 +13,26 @@
 //   Output: [-1,-1]
 
 // stub 
-var findFirstAndLastIndexOfValue = (target) => {
+var findFirstAndLastIndexOfValue = (array, target) => {
+    let maxIndex = -1;
+    let count = 0;
+    let found = false;
+
+    array.map((num, index) => {
+        if (num == target){
+            // console.log('index: ' + index);
+            // console.log('num: ' + num)
+            maxIndex = index;
+            count++;
+            found = true;
+        }
+    });
+    if (found){
+        return [maxIndex-count+1,maxIndex];
+    } 
     return [-1,-1];
 }
+
+console.log(findFirstAndLastIndexOfValue([3,4,7,7,9,9,9,10], 9));
+console.log(findFirstAndLastIndexOfValue([3,4,7,7,9,10], 9));
+console.log(findFirstAndLastIndexOfValue([3,4,7,7,9,9,9,10], 8));
