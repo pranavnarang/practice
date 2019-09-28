6896024c6692995a4a4e25767b4fda99f26d7610
Pranav <pnarang1130@gmail.com>
2019-09-27 17:19:29 -0700

    added stub

---
+++ google-nums.js
@@ -0,0 +1,18 @@
+// Problem [asked by 🏢 Google]:
+// Find the starting and ending position of a given target value in array of integers nums sorted in ascending order.
+
+// If the target is not found in the array, return [-1, -1].
+
+// Your algorithm's runtime complexity must be in the order of O(log n)
+
+// Example:
+                      
+//   Input: nums = [3,4,7,7,9,9,9,10], target = 9
+//   Output: [4,6]
+//   Input: nums = [3,4,7,7,9,9,9,10], target = 8
+//   Output: [-1,-1]
+
+// stub 
+var findFirstAndLastIndexOfValue = (target) => {
+    return [-1,-1];
+}
